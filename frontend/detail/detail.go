830c496369602066d3706c11975b92b65aa5c857
DrCrazy1 <rico3004gmx.de>
2019-11-10 22:02:54 +0100

    Init for activityList Created models folder Added img folder to resources

--- frontend/detail/detail.go
+++ frontend/detail/detail.go
@@ -1,33 +1,15 @@
 package detail
 
 import (
+	"../../models"
 	"html/template"
 	"net/http"
 	"time"
 )
 
-type ActivityDetail struct {
-	Id          uint32
-	Type        string
-	Comment     string
-	Length      float64
-	WaitingTime float64
-	AvgSpeed    float64
-	MaxSpeed    float64
-	DateTime    time.Time
-}
-
 func NewHandler(w http.ResponseWriter, r *http.Request) {
-	detail := ActivityDetail{
-		Id:          1,
-		Type:        "Radfahren",
-		Comment:     "I am a useful comment",
-		Length:      12.3,
-		WaitingTime: 5.2,
-		AvgSpeed:    13.4,
-		MaxSpeed:    17.8,
-		DateTime:    time.Now(),
-	}
+	detail := activityDetail.New(1, "Radfahren", "I am a useful comment",
+		12.3, 5.2, 13.4, 17.8, time.Now())
 
 	t, err := template.ParseFiles("frontend/detail/detail.html")
 
---
+++ models/activityDetail.go
@@ -0,0 +1,57 @@
+package activityDetail
+
+import (
+	"strconv"
+	"time"
+)
+
+type ActivityDetail struct {
+	id          uint32
+	sportType   string
+	comment     string
+	length      float64
+	waitingTime float64
+	avgSpeed    float64
+	maxSpeed    float64
+	dateTime    time.Time
+}
+
+func New(id uint32, sportType string, comment string, length float64, waitingTime float64, avgSpeed float64, maxSpeed float64, dateTime time.Time) ActivityDetail {
+	return ActivityDetail{id: id, sportType: sportType, comment: comment, length: length, waitingTime: waitingTime, avgSpeed: avgSpeed, maxSpeed: maxSpeed, dateTime: dateTime}
+}
+
+func (a ActivityDetail) GetSportType() string {
+	return a.sportType
+}
+
+func (a ActivityDetail) GetWeekDay() time.Weekday {
+	return a.dateTime.Weekday()
+}
+
+func (a ActivityDetail) GetDate() string {
+	return strconv.Itoa(a.dateTime.Day()) + "." + a.dateTime.Month().String() + " " + strconv.Itoa(a.dateTime.Year())
+}
+
+func (a ActivityDetail) GetTime() string {
+	return strconv.Itoa(a.dateTime.Hour()) + ":" + strconv.Itoa(a.dateTime.Minute())
+}
+
+func (a ActivityDetail) GetLength() float64 {
+	return a.length
+}
+
+func (a ActivityDetail) GetComment() string {
+	return a.comment
+}
+
+func (a ActivityDetail) GetAvgSpeed() float64 {
+	return a.avgSpeed
+}
+
+func (a ActivityDetail) GetMaxSpeed() float64 {
+	return a.maxSpeed
+}
+
+func (a ActivityDetail) GetWaitingTime() float64 {
+	return a.waitingTime
+}
