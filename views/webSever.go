27bbf9b2e31143077996eb8a01515aa6f5e4a920
DrCrazy1 <rico3004gmx.de>
2019-11-13 14:09:54 +0100

    Deleted login directory Implemented basic auth in wrapping method

--- views/webSever.go
+++ views/webSever.go
@@ -2,31 +2,36 @@ package views
 
 import (
 	"./detail"
-	"./login"
 	"./overview"
 	"./upload"
+	"fmt"
 	"net/http"
 )
 
 func CreateWebServer() {
 	//Outsource to backend
-	http.HandleFunc("/", login.NewHandler)
-	http.HandleFunc("/overview", basicAuth(overview.NewHandler))
-	http.HandleFunc("/upload/", upload.NewHandler)
-	http.HandleFunc("/detail/", detail.NewHandler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", basicAuth(overview.NewHandler))
+	http.HandleFunc("/upload/", basicAuth(upload.NewHandler))
+	http.HandleFunc("/detail/", basicAuth(detail.NewHandler))
+	fmt.Println(http.ListenAndServe(":8080", nil))
 }
 
 func basicAuth(hf http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		user, pwd, ok := r.BasicAuth()
+		isValid := authenticate(user, pwd)
 
-		if !ok || user != "Rico" || pwd != "1234" {
-			w.Header().Set("WWW-Authenticate", `Basic-Realm="Restricted"`)
-			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-			return
+		if !ok || !isValid {
+			w.Header().Add("WWW-Authenticate", "Basic Realm=\"Strava\"")
+			w.WriteHeader(401)
+		} else {
+			w.WriteHeader(200)
 		}
 
 		hf(w, r)
 	}
+}
+
+func authenticate(username, password string) bool {
+	return username == "Rico" && password == "1234"
 }
