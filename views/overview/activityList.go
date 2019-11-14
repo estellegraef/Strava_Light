2e6d9a2de0b402068745c6f4cc5b3846afae1198
DrCrazy1 <rico3004gmx.de>
2019-11-14 18:49:55 +0100

    Added new background image Serving css and images (public)

--- views/overview/activityList.go
+++ views/overview/activityList.go
@@ -2,6 +2,7 @@ package overview
 
 import (
 	"../../cmd"
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -10,12 +11,16 @@ func NewHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("views/templates/html/index.html"))
 
 	var data []cmd.Activity
-	if r.Method != http.MethodPost {
-		data = cmd.GetActivities()
-	} else {
+	if r.Method == http.MethodPost {
 		search := r.FormValue("search")
 		data = cmd.SearchActivities(search)
+	} else {
+		data = cmd.GetActivities()
 	}
 
-	tmpl.Execute(w, data)
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		_ = fmt.Errorf("Template execution failed! \n %w", err)
+	}
 }
--- views/webSever.go
+++ views/webSever.go
@@ -6,6 +6,7 @@ import (
 	"./upload"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func CreateWebServer() {
@@ -13,6 +14,8 @@ func CreateWebServer() {
 	http.HandleFunc("/", basicAuth(overview.NewHandler))
 	http.HandleFunc("/upload/", basicAuth(upload.NewHandler))
 	http.HandleFunc("/detail/", basicAuth(detail.NewHandler))
+	http.Handle("/styles/", http.StripPrefix(strings.TrimRight("/styles/", "/"), http.FileServer(http.Dir("views/templates/styles"))))
+	http.Handle("/images/", http.StripPrefix(strings.TrimRight("/images/", "/"), http.FileServer(http.Dir("resources/img"))))
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
 
