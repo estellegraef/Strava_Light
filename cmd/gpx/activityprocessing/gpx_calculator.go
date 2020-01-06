c1fd5958c2e6bf9fc6ef5e449ba7717d005234d0
estellegraef <estellegraef@users.noreply.github.com>
2020-01-06 15:58:01 +0100

    Calculation of distance in km with haversine formula added

--- cmd/gpx/activityprocessing/gpx_calculator.go
+++ cmd/gpx/activityprocessing/gpx_calculator.go
@@ -5,6 +5,7 @@ package activityprocessing
 
 import (
 	"Strava_Light/cmd/gpx/gpx_info"
+	"math"
 )
 
 /*important:
@@ -44,7 +45,41 @@ func GetAvgSpeed(points []gpx_info.TrackPoint) float64 {
 	return speedSum / float64(len(points))
 }
 
+//calculate total distance in km by adding up distance between previous trackpoint and current
 func CalculateDistanceInKilometers(points []gpx_info.TrackPoint) float64 {
-	//Harvesine Formula
-	return 0
+	var previousTrkPt gpx_info.TrackPoint
+	var totalDistance float64
+	for index, point := range points {
+		if index == 0 {
+			previousTrkPt = point
+		} else {
+			var DistanceBetweenPoints = CalculateDistance2Points(previousTrkPt.GetLatitude(), previousTrkPt.GetLongitude(), point.GetLatitude(), point.GetLongitude())
+			totalDistance = totalDistance + DistanceBetweenPoints
+			previousTrkPt = point
+		}
+	}
+	return totalDistance
+}
+
+//calculate the distance between 2 points using the haversine formula
+func CalculateDistance2Points(lat1, lon1, lat2, lon2 float64) float64 {
+	radiantLat1 := CalculateRadiant(lat1)
+	radiantLat2 := CalculateRadiant(lat2)
+	radiantLon1 := CalculateRadiant(lon1)
+	radiantLon2 := CalculateRadiant(lon2)
+	differenceLat := radiantLat1 - radiantLat2
+	differenceLon := radiantLon1 - radiantLon2
+
+	haversine := math.Pow(math.Sin(differenceLat/2), 2) + math.Cos(lat1)*math.Cos(lat2)*
+		math.Pow(math.Sin(differenceLon/2), 2)
+
+	c := 2 * math.Atan2(math.Sqrt(haversine), math.Sqrt(1-haversine))
+
+	distanceInKm := c * 6371
+
+	return distanceInKm
+}
+
+func CalculateRadiant(val float64) float64{
+	return val*math.Pi/180
 }
--- cmd/gpx/activityprocessing/gpx_calculator_test.go
+++ cmd/gpx/activityprocessing/gpx_calculator_test.go
@@ -27,3 +27,21 @@ func TestGetAvgSpeed(t *testing.T) {
 	var avgSpeed = GetAvgSpeed(actualTrackPoints)
 	assert.Equal(t, avgSpeed, 6.189253561253568)
 }
+
+//approximate result oriented on https://opensourceconnections.com/blog/uploads/2009/02/clientsidehaversinecalculation.html
+func TestCalculateDistance2Points(t *testing.T) {
+	var file = fileTools.ParseXml(testPath)
+	var actualTrackPoints = GetAllTrackPoints(file)
+	var distance = CalculateDistance2Points(actualTrackPoints[0].GetLatitude(), actualTrackPoints[0].GetLongitude(), actualTrackPoints[1].GetLatitude(), actualTrackPoints[1].GetLongitude())
+	//TODO check w/website: here 0.005045 there 0.005301 km
+	assert.Equal(t, 0.005045829921162091, distance)
+}
+
+//approximate result oriented on https://www.sportdistancecalculator.com/import-gpx.php#map
+func TestCalculateDistanceInKilometers(t *testing.T) {
+	var file = fileTools.ParseXml(testPath)
+	var actualTrackPoints = GetAllTrackPoints(file)
+	var distance = CalculateDistanceInKilometers(actualTrackPoints)
+	//TODO check w/website: here 24.28 km, there 24.71 km
+	assert.Equal(t, 24.28255382563406, distance)
+}
