76f0cc464e51c0af046885010737a72cf0d8e2d7
jassiiihae <meljas97@gmail.com>
2020-01-12 15:41:55 +0100

    Refactored tests

--- backend/webserver/webServer_test.go
+++ backend/webserver/webServer_test.go
@@ -12,11 +12,11 @@ import (
 	"github.com/estellegraef/Strava_Light/backend/auth"
 	"github.com/estellegraef/Strava_Light/backend/user"
 	"github.com/estellegraef/Strava_Light/resources"
+	"github.com/estellegraef/Strava_Light/testStorage"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"path/filepath"
 	"testing"
 	"time"
@@ -149,8 +149,7 @@ func TestHTTPSServer(t *testing.T) {
 }
 
 func TestCheckAndHandleStoragePathWithNonExistPath(t *testing.T) {
-	defaultDir, err := os.Getwd()
-	assert.NoError(t, err)
+	defaultDir := testStorage.GetBasePathTestStorage()
 
 	baseDir := filepath.Join(defaultDir, "test1") // <- Path does not exist
 
@@ -164,8 +163,7 @@ func TestCheckAndHandleStoragePathWithNonExistPath(t *testing.T) {
 }
 
 func TestCheckAndHandleStoragePathWithExistPath(t *testing.T) {
-	defaultDir, err := os.Getwd()
-	assert.NoError(t, err)
+	defaultDir := testStorage.GetBasePathTestStorage()
 
 	checkAndHandleStoragePath(defaultDir, defaultDir)
 
---
+++ testStorage/var_test.go
@@ -0,0 +1,10 @@
+package testStorage
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetBasePath(t *testing.T) {
+	fmt.Printf(GetBasePathTestStorage())
+}
